2f99a1d30e62ecd055d5033e49e0191e18f77c4f
Vladislava Semyannova <demina2000.alexa@gmail.com>
2026-03-26 11:47:45 +0300

    добавлено

--- app/src/main/java/org/example/domain/Protocol.java
+++ app/src/main/java/org/example/domain/Protocol.java
@@ -1,4 +1,22 @@
 package org.example.domain;
 
-public class Protocol {
-}
+import java.time.Instant;
+import java.util.Set;
+
+public final class Protocol {
+    private long id;
+    private String name;
+    private Set<MeasurementParam> requiredParams;
+    private String ownerUsername;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public Protocol(long id, String name, Set<MeasurementParam> requiredParams,
+                    String ownerUsername, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.requiredParams = requiredParams;
+        this.ownerUsername = ownerUsername;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
